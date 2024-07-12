cc7bb7834965cf7ada02d9ddd3a535153aba034c
Jingchao Di <alswlx@gmail.com>
2024-07-13 00:48:55 +0800

    feat: add basic album view for song
    commit de050d8813a59cd66a088844543b50ae6fdf5fdb
    Author: Jingchao Di <alswlx@gmail.com>
    Date:   Sat Jul 13 00:48:35 2024 +0800
    
        feat: album works
    
    commit 98834322ea06a71b0b0c03bc1401da06eccb42ff
    Author: Jingchao Di <alswlx@gmail.com>
    Date:   Sat Jul 13 00:35:52 2024 +0800
    
        feat: add basic page view for song

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,8 @@
 import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import React from 'react';
 
 export default function Layout() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
+    <Outlet />
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
-import yayJpg from '../assets/yay.jpg';
+import React from 'react';
+import {
+  ArrowRightOutlined,
+  CaretRightOutlined,
+  EditOutlined,
+  EllipsisOutlined, ExportOutlined, InfoOutlined,
+  RightCircleOutlined,
+  SettingOutlined, StepForwardOutlined,
+} from '@ant-design/icons';
+import { Avatar, Card, Col, Flex, Row } from 'antd';
+
+const { Meta } = Card;
 
 export default function HomePage() {
-  return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
-  );
+
+
+  return <Row justify="center">
+    <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+      <Card
+        style={{ width: 300 }}
+        cover={
+          <img
+            src="https://e25ba8-log4d-c.dijingchao.com/202302/1675664205664-15960011-481e-4736-ad81-617656f3d315.png"
+          />
+        }
+        actions={[
+          <a href={'https://music.apple.com/cn/album/viva-la-vida-or-death-and-all-his-friends/1122773394?l=en'}
+             target={'_blank'}>
+            <CaretRightOutlined key="play" />,
+          </a>,
+          <StepForwardOutlined key={'next'} />,
+          <a href={'https://music.douban.com/subject/3040149/'} target={'_blank'}>
+            <InfoOutlined key={'info'}>
+            </InfoOutlined>,
+          </a>,
+        ]}
+      >
+        <Meta
+          title="Viva La Vida"
+          description="Coldplay"
+        />
+      </Card>
+    </Col>
+  </Row>;
+
 }

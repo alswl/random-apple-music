5e9254a0f3682fe92f79d2d8c8585b6191dc7920
Jingchao Di <alswlx@gmail.com>
2024-07-13 16:55:37 +0800

    fix: umi required plugin enable

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   routes: [{ path: '/', component: 'index' }],
   npmClient: 'pnpm',
-  plugins: ['@umijs/plugins/dist/antd'],
+  plugins: ['@umijs/plugins/dist/antd', '@umijs/plugins/dist/analytics'],
   antd: {},
   title: 'Random Apple Music',
   favicons: ['/favicon.svg'],

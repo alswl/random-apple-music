c51a9807800dcf05ca305dde585cd06747103411
Jingchao Di <alswlx@gmail.com>
2024-07-13 16:51:32 +0800

    feat: enable google analytics

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   antd: {},
   title: 'Random Apple Music',
   favicons: [
-    // 此时将指向 `/favicon.png` ，确保你的项目含有 `public/favicon.png`
     '/favicon.svg',
   ],
+  analytics: {
+    ga_v2: 'G-VHWBKY6N6F', // Google Analytics 的 key (GA 4)
+  },
 });
